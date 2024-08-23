3bd71b98c18709e8a68f76e27ed7d36e2bcf4373
Vedant Alimchandani <vedant.alimchandani.c@netradyne.com>
2024-08-23 15:28:38 +0530

    implemented Persistence

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
-import React from 'react';
-import {Provider} from 'react-redux';
-import logger from 'redux-logger';
-import {createStore, applyMiddleware} from 'redux';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import Todo from './component/Todo';
 import rootReducer from './redux/reducers/rootReducer';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    const loggerMiddleware = createLogger();
+    const enhancer = applyMiddleware(thunkMiddleware, loggerMiddleware);
 
-const App = () => (
-  <Provider store={store}>
-    <Todo />
-  </Provider>
-);
+    const persistConfig = {
+      key: 'root',
+      storage: AsyncStorage,
+      whitelist: ['todos']
+    };
+
+    const persistedReducer = persistReducer(persistConfig, rootReducer);
+    const store = createStore(persistedReducer,{}, enhancer);
+    const persistor = persistStore(store);
+
+    
+    return (
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Todo />
+        </PersistGate>
+      </Provider>
+    );
+  }
+}
 
 export default App;
